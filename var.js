54fa8045a501961ba2454b99f0d728a3063ed7e5
Sayon-okai <sayon.study@gmail.com>
2024-10-14 21:20:00 +0000

    added my pet name is

--- var.js
+++ var.js
@@ -12,6 +12,16 @@ console.log("My dog",petDog);
 
 
 var petCat;
-var petCat = "pepper";
+var petCat = "Pepper";
 console.log("My cat", petCat)
+
+// The petDog name console output concatenation
+
+console.log("My pet's dog name is", petDog)
+
+// The petCat name console output concatenation
+
+console.log("My pet's Cat name is", petCat)
+
+
 
