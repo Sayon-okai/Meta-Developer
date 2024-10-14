466a657c0f5268089e8e6b56d805499d16f9edce
Sayon-okai <sayon.study@gmail.com>
2024-10-14 20:39:05 +0000

    I have just added concate js file

---
+++ concate.js
@@ -0,0 +1,12 @@
+/* 
+Concatenation in JavaScript
+_______________________________________________
+*/
+
+/* Joining three peices of text using using + character  */
+
+console.log("Hello "+" there, "+" world");
+
+/* Joining three peices of text using using , character  */
+console.log("Hello", "there,"," Boss");
+
