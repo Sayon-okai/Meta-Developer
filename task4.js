2fc3a66fe8bdb3dd457a5f3bea1f0806cb8dafb3
Sayon-okai <sayon.study@gmail.com>
2024-10-16 22:14:24 +0000

    Added a line of code that add two number and printout the result in the console

---
+++ task4.js
@@ -0,0 +1,13 @@
+/* 
+
+   Task 4: Add numbers using the + operator
+Console log the result of adding two numbers, 5 and 10, using the + operator.
+Note: This task should be completed on a single line of code. The output in the console should be 15.
+
+
+*/
+
+// this is adding 5 and 10 and printing using console.log 
+// The code below is only correct if the result is equal 15
+
+console.log(5 + 10);
