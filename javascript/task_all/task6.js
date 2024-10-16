659a58cef36d931bffdf2eabdc7ec0cf2d46ec65
Sayon-okai <sayon.study@gmail.com>
2024-10-16 22:38:49 +0000

    counter is equal to 8, code is tested

---
+++ javascript/task_all/task6.js
@@ -0,0 +1,22 @@
+/* 
+   Task 6: Use the += operator to accumulate values in a variable
+Code a new variable and name it counter, assigning it to the value of 0.
+On the next line, use the += operator to increase the value of counter by 5.
+On the next line, use the += operator to increase the value of counter by 3.
+On the fourth line, console log the value of the counter variable.
+Note: The output value should be 8.
+
+*/
+
+// Declaring variable counter
+var counter = 0;
+// Increasing counter by 5
+counter += 5;
+// Increasing counter by 3
+counter += 3;
+
+// Printing the output using console.log
+    console.log(counter);
+
+// Counter is now 8
+// See by running code 
