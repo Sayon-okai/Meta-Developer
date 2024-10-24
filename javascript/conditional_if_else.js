1c10468db018c47ad8cb45b441c9f1ec30b06da5
Sayon-okai <sayon.study@gmail.com>
2024-10-24 17:41:25 +0000

    Have added conditional statement to conditional_if_else

---
+++ javascript/conditional_if_else.js
@@ -0,0 +1,18 @@
+// Conditional flow statement on student Grade output
+/* 
+   Checking if a student result for the exam is greater than or equal to 70
+   pass is => 70
+   fail is < 70
+*/
+
+// var block
+
+var result = 50;
+
+// Statement
+if(result > 49 ){
+    console.log("You have passed the exam, your score is")
+}else {
+    console.log("You have Failed the exam, your score is")
+        
+} 
