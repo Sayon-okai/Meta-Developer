fccc6336666f5ca8b03d86de7a12cb983882ff4e
Sayon-okai <sayon.study@gmail.com>
2024-10-24 17:44:16 +0000

    code bug have been found and resolved conditional_if_else

--- javascript/conditional_if_else.js
+++ javascript/conditional_if_else.js
@@ -11,8 +11,10 @@ var result = 70;
 
 // Statement
 if(result > 69 ){
+    // IF block
     console.log("You have passed the exam, your score is", result)
 }else {
+    // Else block
     console.log("You have Failed the exam, your score is")
         
 } 
