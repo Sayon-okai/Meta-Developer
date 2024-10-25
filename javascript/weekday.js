7a36b07313efb1d4cb4a3a7f73b6033df65bf7cb
Sayon-okai <sayon.study@gmail.com>
2024-10-25 13:45:46 +0000

    Code block added to  weekday.js and working correctly

--- javascript/weekday.js
+++ javascript/weekday.js
@@ -12,3 +12,30 @@ Finally, update the console.log calls for each case, based on whatever activity 
 
 */
 
+// Declaring the var block
+
+var day = "Sunday";
+
+switch (day == "Sunday") {
+    case "Monday":
+        console.log("Today is Monday");
+        break;
+    case "Tuesday":
+        console.log("Today is Tuesday");
+    case "Wednesday":
+        console.log("Today is Wednesday");
+        break;
+    case "Thursday":
+        console.log("Today is Thursday");
+    case "Friday":
+        console.log("Today is Friday");
+        break;
+    case "Saturday":
+        console.log("Today is Saturday");
+    case "Saturday":
+            console.log("Today is Saturday");
+            break;
+    
+    default:
+        console.log("There is no such case") ;        
+}
