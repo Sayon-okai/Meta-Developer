5efd0de2a87045b00d4f7ab3756a3a834a11b041
Sayon-okai <sayon.study@gmail.com>
2024-10-31 09:01:17 +0000

    I have added the code for task 1 and it is tested and working

---
+++ javascript/task_all/loops/for_loop/task1.js
@@ -0,0 +1,12 @@
+/* 
+   ######
+   Task: Write a for loop that counts from
+         1 to 5, and display the text Counting Complete
+         after the count in the console.
+
+*/
+
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
+console.log('Counting Completed!');
