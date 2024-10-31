330e65fecae41eb039ff6f7d65d1980d8a6b4996
Sayon-okai <sayon.study@gmail.com>
2024-10-31 09:16:55 +0000

    task problem set added

---
+++ javascript/task_all/loops/for_loop/task2.js
@@ -0,0 +1,10 @@
+/* 
+   ######
+   Task: Write a for loop that counts from
+         5 to 1, and display the text Counting Complete
+         after the count in the console.
+
+*/
+// i decreament ###### count down
+
+
