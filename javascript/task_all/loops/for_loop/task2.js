ec07a8b5fa6471b7189945087ebd47318d3fc636
Sayon-okai <sayon.study@gmail.com>
2024-10-31 13:50:19 +0000

    task2 finished and working

--- javascript/task_all/loops/for_loop/task2.js
+++ javascript/task_all/loops/for_loop/task2.js
@@ -1,10 +1,16 @@
 /* 
    ######
    Task: Write a for loop that counts from
-         5 to 1, and display the text Counting Complete
+         5 to 1, and display the text Countdown finished
          after the count in the console.
 
 */
 // i decreament ###### count down
+
+for (let i = 5; i >= 1; i--) {
+    console.log(i);
+
+}
+console.log('Countdown Finished');
 
 
