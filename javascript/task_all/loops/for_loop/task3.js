6cac6eb88056e9dd3022b7c315c085c45f73091f
Sayon-okai <sayon.study@gmail.com>
2024-11-01 00:04:18 +0000

    Track recod

---
+++ javascript/task_all/loops/for_loop/task3.js
@@ -0,0 +1,13 @@
+/* 
+   ###################
+   While loop increament
+*/
+
+var i = 5;
+
+while (i > 0 ) {
+       console.log(i);
+       i = i + 1;
+      break;
+}
+console.log('Counting completed!')
