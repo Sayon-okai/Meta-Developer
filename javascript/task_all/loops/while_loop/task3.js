ba8316d091dd754467eb5a7fd85ec6fe2340a009
Sayon-okai <sayon.study@gmail.com>
2024-11-01 01:29:19 +0000

    I have moved task3 and task4 to from for_loopdir to while_loop dir

---
+++ javascript/task_all/loops/while_loop/task3.js
@@ -0,0 +1,13 @@
+/* 
+   ###################
+   While loop increament
+*/
+
+var i = 1;
+
+while (i <= 5 ) {
+       console.log(i);
+       i = i + 1;
+      
+}
+console.log('Counting completed!');
