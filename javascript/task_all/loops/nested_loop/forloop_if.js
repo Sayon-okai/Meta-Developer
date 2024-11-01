0635baaac4cfc55476a2a9c5f81dca9c339eba40
Sayon-okai <sayon.study@gmail.com>
2024-11-01 04:24:28 +0000

    forloop_if.js solution have been added and tested code green

--- javascript/task_all/loops/nested_loop/forloop_if.js
+++ javascript/task_all/loops/nested_loop/forloop_if.js
@@ -32,3 +32,17 @@ Bronze medal
 
 */
 
+for (var i = 1; i <= 10; i++) {
+    if (i === 1) {
+        console.log('Gold medal');
+    } else if (i === 2) {
+           console.log('Silver medal');
+    } else if (i === 3) {
+           console.log('Bronze medal');
+    } else {
+        // This code run if the condition is false
+         console.log(i);
+    }
+}
+
+// End of code
