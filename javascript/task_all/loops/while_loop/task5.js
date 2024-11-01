4f6cbf653808877c56db90c3ddee2122f18f8ff8
Sayon-okai <sayon.study@gmail.com>
2024-11-01 01:35:33 +0000

    I have added the problem set for task5.js

---
+++ javascript/task_all/loops/while_loop/task5.js
@@ -0,0 +1,12 @@
+/* 
+   ###################
+   While loop increament 
+   for the example below
+console.log(2018)
+console.log(2019)
+console.log(2020)
+console.log(2021)
+console.log(2022)
+
+*/
+
