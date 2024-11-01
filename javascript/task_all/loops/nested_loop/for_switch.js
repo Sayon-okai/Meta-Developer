c89aa67309dc1b691890f2e9aedca943eceb4d54
Sayon-okai <sayon.study@gmail.com>
2024-11-01 05:01:35 +0000

    I have added the problem set for for_switch.js file

---
+++ javascript/task_all/loops/nested_loop/for_switch.js
@@ -0,0 +1,8 @@
+/*
+Exercise 2. Use the completed code from the previous task, but convert the conditionals to a switch statement.
+When you code the solution, the output in the console should remain exactly the same as in the previous question.
+
+Note: You'll need three separate cases for the three medals, and a default case for all other values of the i variable
+
+*/
+
