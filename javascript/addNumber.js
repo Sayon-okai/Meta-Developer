c0c424f5599425a617ba76b3964290978508edcf
Sayon-okai <sayon.study@gmail.com>
2024-11-03 15:54:58 +0000

    A code the add two given number and print the ouput

---
+++ javascript/addNumber.js
@@ -0,0 +1,13 @@
+/* 
+######################
+A function that add two given number
+#############################
+*/
+
+function addTwoNumbers(num1, num2) {
+    return num1 + num2;
+}
+
+const result = addTwoNumbers(5,6);
+
+console.log(result);
