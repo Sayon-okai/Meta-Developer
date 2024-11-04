8c58ed9227abe5272cd82cb36ffad5cc53d42ab7
Sayon-okai <sayon.study@gmail.com>
2024-11-04 13:11:26 +0000

    Using object literal and dot notation to built an object with key value pairs

---
+++ javascript/arrays/userObject.js
@@ -0,0 +1,12 @@
+/*
+  #####################
+    Using object literal and dot notation to built an object with key value pairs
+  #####################
+ 
+*/
+
+var user = {}
+user.user_id = 5969;
+user.username = 'sayon-okai';
+user.email = 
+
