188e4dd9be769fbbe963a05bbda5e896c2d6be19
Sayon-okai <sayon.study@gmail.com>
2024-11-04 13:03:08 +0000

    myfav color is working

---
+++ javascript/arrays/myfav_color.js
@@ -0,0 +1,18 @@
+/*
+   My favourite colors
+*/
+
+
+const colors = ['red', 'blue', 'green', 'aqua']
+
+// Adding element to colors
+colors.push('white');
+// Accessing all element in the array
+console.log(colors);
+// Accessing color by index in the array
+// console.log(colors[0]);
+// console.log(colors[1]);
+// console.log(colors[3]);
+// console.log(colors[2]);
+
+
