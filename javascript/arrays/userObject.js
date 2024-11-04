cc54bd1a22193e1b103c90382b19f6dc12863d13
Sayon-okai <sayon.study@gmail.com>
2024-11-04 14:16:42 +0000

    userObject.js main fixed code status green

--- javascript/arrays/userObject.js
+++ javascript/arrays/userObject.js
@@ -3,27 +3,14 @@
     Using object literal and dot notation to built an object with key value pairs
   #####################
  
+
 */
 
-// var user = {}
-// user.user_id = 5969;
-// user.username = 'sayon-okai';
-// user.email = 'sayon@gmail.com';
-// user.password = 'userpassword';
-// user.first_name = 'Sayon';
-// user.last_name = 'Okai';
 
-// for (var i = 0; i < user.length; i++) {
-//   console.log(user[i]);
-// }
+/*
+Object.keys(user) to get an array of the keys and then loop through them. Here's a corrected version of your code:
 
-var user = {};
-user.user_id = 5969;
-user.username = 'sayon-okai';
-user.email = 'sayon@gmail.com';
-user.password = 'userpassword';
-user.first_name = 'Sayon';
-user.last_name = 'Okai';
+*/
 
 for (var i = 0; i < Object.keys(user).length; i++) {
   var key = Object.keys(user)[i];
