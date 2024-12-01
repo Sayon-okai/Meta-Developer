f0eaa1354f48a3daa3f372e08d966ab0c77f3926
Sayon-okai <sayon.study@gmail.com>
2024-12-01 18:03:16 +0000

    A program that give you the current when today is passed as the parameter value

---
+++ tester/whatday.js
@@ -0,0 +1,17 @@
+// A function that get today date
+
+
+
+function today (day) {
+
+        if (day === today) {
+            return new Date ()
+        } else {
+            console.log("You are wrong")
+        }
+}
+
+today(today);
+
+
+
