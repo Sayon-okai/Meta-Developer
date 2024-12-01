a728f362f6804a21e8f575365eba51cdd621ce46
Sayon-okai <sayon.study@gmail.com>
2024-12-01 19:26:13 +0000

    There are two keywords that are essential for OOP with classes in JavaScript.

---
+++ javascript/OO_program.js
@@ -0,0 +1,22 @@
+/* 
+
+Specifically, I'm preparing to build the following inheritance hierarchy:
+
+                  Animal
+                  /    \
+             Cat       Bird
+          /               \          \
+    HouseCat   Tiger      Parrot
+
+There are two keywords that are essential for OOP with classes in JavaScript.
+
+These keywords are extends and super.
+
+The extends keyword allows me to inherit from an existing class.
+
+Based on the above hierarchy, I can code the Animal class like this:
+
+
+
+*/
+
