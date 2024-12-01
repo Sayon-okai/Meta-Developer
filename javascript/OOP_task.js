dd0193be99c48b0a5ff288f4ede1dfd69f30e5be
Sayon-okai <sayon.study@gmail.com>
2024-12-01 17:35:53 +0000

    Testing polymorphism -- OOP - functions called -- 100%

--- javascript/OOP_task.js
+++ javascript/OOP_task.js
@@ -11,16 +11,24 @@ class Animal {
 };
 
 class Human extends Animal {
+    
       useHands() {
+// Human accessing the useHands method from Animal the super class
+         super.useHands()
           console.log("Human use hand to , feeed, hold things, clap and more")
       }
-}
+};
 
 class Bat extends Animal {
      useHands() {
         console.log("Use hand to feed only")
      }
-}
+};
 
+var babyHuman = new Human();
+var babyBat = new Bat();
+
+babyHuman.useHands();
+babyBat.useHands();
 
 
