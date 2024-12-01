c4c4d7e8697ea722b46e922984efb01c31ff7737
Sayon-okai <sayon.study@gmail.com>
2024-12-01 20:07:50 +0000

    Cat added as a sub class -- method added

--- javascript/OO_program.js
+++ javascript/OO_program.js
@@ -19,4 +19,58 @@ Based on the above hierarchy, I can code the Animal class like this:
 
 
 */
+// Animal is the prototype object 
+class Animal {
+    constructor (color = 'yellow', energy = 100) {
+    this.color = color;
+    this.energy = energy;
+    }
+//  isActive
+  isActive() {
+    // Checking is the Animal is active or sleeping 
+    // Animal is active if Animal energy is greater then 0
+    if(this.energy > 0) {
+        this.energy -= 20;
+        console.log('Energy is decreasing, currently at:', this.energy);
+    } 
+    // checking if Animal is as sleep 
+    else if (this.energy == 0) {
+        this.sleep();
+    }
+}
+
+    sleep() {
+        this.energy += 20;
+        console.log('Energy is increasing, currently at:', this.energy)
+    }
+
+    getColor() {
+        console.log(this.color)
+    }   
+}
+
+/* 
+   Each animal object, no matter which one it is, will share the properties of color and energy.
+
+    Now I can code the Cat and Bird classes:
+
+*/
+
+// Sub class of Animal
+class Cat extends Animal {
+   constructor(sound = 'purr', canJumpHigh = true, canClimbTrees = true, color, energy) {
+        super(color, energy);
+        this.sound = sound;
+        this.canClimbTrees = canClimbTrees;
+        this.canJumpHigh = canJumpHigh;
+
+   }
+//    Adding method on self
+makeSound() {
+    console.log(this.sound);
+}
+}
+
+// Bird sub class of Animal
+
 
