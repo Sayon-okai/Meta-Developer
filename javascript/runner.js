a57ccac7ad0783c98f3ad1e85ef33b258d27afde
Sayon-okai <sayon.study@gmail.com>
2024-12-05 13:50:54 +0000

    Person object OOP task

--- javascript/runner.js
+++ javascript/runner.js
@@ -1,17 +1,20 @@
-var score = prompt('What is your score'); // Example score
-
-if (score >= 0 && score <= 100) { // Check if the score is valid
-    if (score >= 90) {
-        console.log("Grade: A");
-    } else if (score >= 80) {
-        console.log("Grade: B");
-    } else if (score >= 70) {
-        console.log("Grade: C");
-    } else if (score >= 60) {
-        console.log("Grade: D");
-    } else {
-        console.log("Grade: F");
-    }
-} else {
-    console.log("Invalid score. Please enter a score between 0 and 100.");
+class Person {
+    constructor(name = 'John', age = 25)
+ {
+        this.name = name;
+        this.age = age;
+   }
+introduce() {
+      return `Hi, my name is ${this.name} and I am ${this.age} years old.`;
 }
+celebrateBirthday() {
+     this.age += 1;    
+    return `Happy Birthday, ${this.name}! you are now ${this.age} years old!`;
+}
+}
+
+const Mary = new Person ('Mary Brown', 35);
+
+// running all prototype method on mary 
+console.log(Mary.introduce());
+console.log(Mary.celebrateBirthday());
