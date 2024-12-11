9d785f940dde0f5aafa50d408360e796e2e6fc89
Sayon-okai <sayon.study@gmail.com>
2024-12-11 16:02:40 +0000

    Programming Assignment: Array and object iteration

--- javascript/solution.js
+++ javascript/solution.js
@@ -1,36 +1,51 @@
-// Task 1
-var dairy = ['cheese', 'sour cream', 'milk', 'yogurt', 'ice cream', 'milkshake']
-function logDairy() {
-    for (let item of dairy) {
-           console.log(item)
-       }
-}
-   
-logDairy();
-// Task 2
-const animal = {
-    canJump: true
-}
+ // Task 1
 
-const bird = Object.create(animal);
-bird.canFly = true;
-bird.hasFeathers = true;
+ var dairy = ['cheese', 'sour cream', 'milk', 'yogurt', 'ice cream', 'milkshake']
 
-function birdCan() {
-    // Prop is for properties of bird
-    for (let prop of Object.keys(bird)) {
-        console.log(prop, ":" , bird[prop]);
+ function logDairy() {   
+ 
+  for ( const items of dairy) {        
+ 
+ console.log(items)
+ 
+     }
+ 
+ }
+ 
+ logDairy();
+ 
+ // Task 2
+ 
+ const animal = {canJump: true};
+ 
+ const bird = Object.create(animal);
+ 
+ bird.canFly = true;
+ 
+ bird.hasFeathers = true;
+ 
+ function birdCan() {   
+ 
+  for (prop of Object.keys(bird)) {       
+ 
+  console.log(`${prop}: ${bird[prop]}`); 
+ 
     }
-}
-
-birdCan();
-
-// Task 3
-function animalCan() {
-    for (let prop  in bird) {
-        console.log(prop, ":" ,bird[prop]);
-    }
-}
-
-animalCan();
-
+ 
+ }
+ 
+ birdCan();
+ 
+ // Task 3
+ 
+ function animalCan() {
+ 
+     for (prop in bird) { 
+ 
+        console.log(`${prop}: ${bird[prop]}`);
+ 
+     }
+ 
+ }
+ 
+ animalCan()
